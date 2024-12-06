098c52c261062ccc2cbf249357e69f6d17b2afa9
sabre <asabre5073@gmail.com>
2024-12-06 23:51:42 +0530

    w

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+// eslint.config.js
+module.exports = {
+    extends: ['eslint:recommended'],
+    parserOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    rules: {
+      'no-console': 'warn',
+      'no-unused-vars': 'warn',
+    },
+  };
+  
